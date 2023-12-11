3df5dd86c0c3dfdc5dca1397dfc567b833641c07
pochy <kenta.teruteru@gmail.com>
2023-12-11 20:51:48 +0900

    日付のタイムゾーン

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,8 +1,9 @@
 export const formatDate = (date: string) => {
-  const formattedDate = new Date(date).toLocaleDateString("ja-JP", {
+  const formattedDate = new Date(date).toLocaleString("ja-JP", {
     year: "numeric",
     month: "long",
     day: "numeric",
+    timeZone: "UTC",
   });
   return formattedDate;
 };
